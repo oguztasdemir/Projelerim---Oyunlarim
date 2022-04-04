b3f1d3d4ef665a14957bacac2ab030cf845925c5
Oğuz Taşdemir <93481181+oguztasdemir@users.noreply.github.com>
2022-04-04 12:00:45 +0300

    Add files via upload

---
+++ Projeler/Gardrops Fonksiyonel Takip Botu/follow_main.py
@@ -0,0 +1,4 @@
+import follow,time
+
+follow.Browser("https://www.gardrops.com/member/general/login")
+time.sleep(2)
---
+++ Projeler/Gardrops Fonksiyonel Takip Botu/kullanıcı.py
@@ -0,0 +1,10 @@
+
+user_list = ["eefsane", "eefsane1", "ataturkcubiri","gunesgozlukleri","baybayanayakkabi","Gulerinki","alaragardropsum125","sahibinden55","ilknurkucukguclu","sebimoda", "MarkadaUcuzluk","seliiaksesuar","sedascarffs","Avon_Gardrops","kitopya","Esra_kavak","oriflamebilgehan","pelinkara0","Asdafasdaf"]
+
+nickname = "" #Girilecek hesabın kullanıcı adı
+password = "" #Girilecek hesabın şifresi
+kisi = 3050 #kaç kişiye takip atılacağını buraya giriniz (3050den fazla girmemenizi öneririm)
+scroll = 180 #sayfayı kaç kere aşağı kaydırmasını istiyorsanız buraya girin (200den fazla girmemenizi öneririm)
+hesap = 6 #listedeki kaç hesaba bakmasını istiyorsanız onu girin
+hesap_sayac = 7 #listedeki kaçıncı elemandan başlamasını istiyorsanız onu girin
+

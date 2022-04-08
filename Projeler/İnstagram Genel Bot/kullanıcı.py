99acaac7c760d3cfca52f36dd8be6bab2d683978
Oğuz Taşdemir <93481181+oguztasdemir@users.noreply.github.com>
2022-04-09 02:31:14 +0300

    Delete Projeler/İnstagram Genel Bot directory

--- Projeler/İnstagram Genel Bot/kullanıcı.py
+++
@@ -1,7 +0,0 @@
-nickname = ""  #Siteye giriş kısmı için fake hesabın nicknamesi
-sifre = "" #Siteye giriş kısmı için fake hesabın şifresi
-kullanıcı = "" #Geri takip kısmında hesaba girebilmek için şifre
-ksifre = ""  #Geri takip kısmında hesaba girebilmek için şifre
-takip = ""  #siteye girişte hesaba takip atılabilmesi için girilecek olan hesabın nicknamesi
-link = "" #Beğeni,yorum,izlenme,kaydetme atabilmek için gönderinin urlsini girin
-sayi = 150  #Siteye giriş kısmında hesaba her site tarafından kaçar tane beğeni/takip/izlenme/yorum/kaydetme atmak istiyorsanız o miktarı girin
